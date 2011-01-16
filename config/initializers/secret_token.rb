d2496987de5ee3c4b1829764e1d5cdd82b56ed8c
Sujal Shah <sujal@forchesoftware.com>
2011-01-15 23:31:49 -0500

    adding initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sgnl::Application.config.secret_token = 'b22e1ff274288627621ab95137dfb9cec91a905695d1368651dd14a9429866760b5eec69ab5fff238a87f288f44f2e65481695f2e1cf3f8eefbd528c9dfd958b'
